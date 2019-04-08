8bfe176e68fac97d4ef68fa6610bb8cfc79644c2
Fabio Costa <fabiotc@gmail.com>
2019-04-08 17:14:15 +0200

    Define Converter and OutboxEntry interfaces

---
+++ src/Outbox/Converter.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dsantang\DomainEventsDoctrine\Outbox;
+
+use Dsantang\DomainEvents\DomainEvent;
+
+interface Converter
+{
+    public function convert(DomainEvent $domainEvent) : OutboxEntry;
+}
